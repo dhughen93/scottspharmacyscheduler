1a1c5bcb6cd89b3b50e22bfbf1faadadd3462740
dhughen93 <dhughen93@gmail.com>
2025-02-15 09:46:11 -0600

    initial commit

---
+++ src/main/java/com/dhughen/scottspharmacyscheduler/ScottspharmacyschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.dhughen.scottspharmacyscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScottspharmacyschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScottspharmacyschedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhughen/scottspharmacyscheduler/ScottspharmacyschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhughen.scottspharmacyscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScottspharmacyschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
